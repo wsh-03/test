7dd0656893525a0d4b8530b9e90793d67d71a7ff
wsh <wsh347115683@gmail.com>
2025-04-02 00:04:41 +0100

    fix commit

--- translate.py
+++ translate.py
@@ -1,7 +1,5 @@
 import os
-from pathlib import Path
 from file_utility import FileProcessor
-<<<<<<< HEAD
 import gpt_translation
 
 def translate(path2folder):
@@ -23,7 +21,7 @@ def translate(path2folder):
                             print(f"Filename: {file_name}")
                             print(f"Content:\n{file_content}")
                         else:
-                            raise Exception ("Error encountered during reading file content or retrieving filename.")                     
+                            raise ValueError ("Error encountered during reading file content or retrieving filename.")                     
 
                 translation_result = gpt_translation.gpt_translate(file_content, None)
                 print(translation_result)
@@ -41,53 +39,8 @@ def translate(path2folder):
     else:
         print(f"ERROR: {path2folder} not found")
         return False
-=======
 
-class CodeTranslator:
-    FILE_TYPE = ".c"
-    
-    def translate(self, path2folder):
-        file_class = FileProcessor()
-        # Check if the provided path is a valid directory
-        if (os.path.isdir(path2folder)):
-            print("\nPath checked successfully\n")
-            for dir, subdirs, files in os.walk(path2folder):      
-                for file in files:
-                    output_dir = path2folder + f"/d_{file}/"
-                    print(os.path.join(dir, file))
-                    if file.endswith(self.FILE_TYPE):
-                        path2file = os.path.join(dir, file)
-                        file_result = file_class.get_file_info(path2file, self.FILE_TYPE)
-                        if file_result is not None:
-                            file_content, file_name = file_result
-                            if file_content is not None and file_name is not None:
-                                print(f"Filename: {file_name}")
-                                print(f"Content:\n{file_content}")
-                            else:
-                                raise Exception ("Error encountered during reading file content or retrieving filename.") 
-                        
-                        translation_result = gpt_translation.gpt_translate(file_content, None)
-                        print(translation_result)
-                        
-                        # Remove comments
-                        clean_code = file_class.remove_comments(translation_result)                        
-                        print(clean_code)
-                        
-                        # Write translated code into correct Rust format
-                        rust_bn = os.path.splitext(file)[0] + ".rs"
-                        with open(output_dir + rust_bn, "w") as f:
-                            f.write(clean_code)
-            print ("Translation Successfully Completed")
-            return True
-        else:
-            print(f"ERROR: {path2folder} not found")
-            return False
-        
->>>>>>> 872c65e05feb376016618439b3e51153b1daa37d
-
-if __name__ == "__main__":
-    translator = CodeTranslator()
-    path2folder = "/home/wsh/test/connector"
-    print(translator.translate(path2folder))
+path2folder = "/home/wsh/test/connector"
+print(translate(path2folder))
 
 
